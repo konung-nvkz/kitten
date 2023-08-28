9abfb36ac7a9355ebd0fd312340f683f060fe54b
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-08-28 12:23:40 +0700

    kittenBase

---
+++ kitten-app/src/components/kittenBase.js
@@ -0,0 +1,33 @@
+import {v4} from 'uuid';
+
+const kittensBase = [
+    {
+        id: v4(),
+        name: 'Мурзик',
+        age: 2,
+        gender: 'male',
+        img: 'cat1.jpg',
+        isAdopted: false,
+        coordinatorPhone: '+7 (123) 456-7890'
+    },
+    {
+        id: v4(),
+        name: 'Барсик',
+        age: 1,
+        gender: 'male',
+        img: 'cat2.jpg',
+        isAdopted: true,
+        coordinatorPhone: '+7 (123) 456-7890'
+    },
+    {
+        id: v4(),
+        name: 'Флорида',
+        age: 3,
+        gender: 'female',
+        img: 'cat3.jpg',
+        isAdopted: true,
+        coordinatorPhone: '+7 (123) 456-7890'
+    },
+];
+
+export default kittensBase
